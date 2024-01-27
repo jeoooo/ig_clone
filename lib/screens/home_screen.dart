f6b45b19bc7c5c8abd5dcfb3c17caa08da8f272a
JEO_CARLO LUBAO <jeocarlolubao@gmail.com>
2024-01-27 18:11:20 +0800

    Refactor home screen and remove bottom navigation bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,20 @@
 // ignore_for_file: no_leading_underscores_for_local_identifiers, prefer_const_constructors, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
-import 'package:ig_clone/screens/widgets/bottom_navbar.dart';
-import 'package:ig_clone/screens/widgets/user_posts.dart';
 
-import 'widgets/stories_bubble.dart';
+import 'news_feed.dart';
+
 import 'widgets/top_appbar.dart';
+
+final List people = [
+  'kobe',
+  'juan',
+  'jessica',
+  'kim',
+  'peter',
+  'john',
+  'bruce'
+];
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -15,14 +24,19 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final List people = [
-    'kobe',
-    'juan',
-    'jessica',
-    'kim',
-    'peter',
-    'john',
-    'bruce'
+  int _selectedIndex = 0;
+  void _navigateBottomNavBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List _children = [
+    newsFeed(people: people),
+    Center(child: Text('search')),
+    Center(child: Text('reels')),
+    Center(child: Text('shop')),
+    Center(child: Text('account')),
   ];
   @override
   Widget build(BuildContext context) {
@@ -31,42 +45,19 @@ class _HomeScreenState extends State<HomeScreen> {
         preferredSize: Size.fromHeight(kToolbarHeight),
         child: topAppbar(),
       ),
-      body: Column(
-        children: [
-          storiesBar(people: people),
-          Expanded(
-            child: ListView.builder(
-                scrollDirection: Axis.vertical,
-                itemCount: people.length,
-                itemBuilder: (context, index) {
-                  return UserPosts(text: people[index]);
-                }),
-          )
+      body: _children[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _navigateBottomNavBar,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'search'),
+          BottomNavigationBarItem(icon: Icon(Icons.video_call), label: 'reels'),
+          BottomNavigationBarItem(icon: Icon(Icons.shop), label: 'shop'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'account'),
         ],
       ),
-      bottomNavigationBar: bottomNavbar(),
-    );
-  }
-}
-
-class storiesBar extends StatelessWidget {
-  const storiesBar({
-    super.key,
-    required this.people,
-  });
-
-  final List people;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 130,
-      child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount: people.length,
-          itemBuilder: (context, index) {
-            return storiesBubble(text: people[index]);
-          }),
     );
   }
 }
---
+++ lib/screens/news_feed.dart
@@ -0,0 +1,52 @@
+// ignore_for_file: no_leading_underscores_for_local_identifiers, prefer_const_constructors, sized_box_for_whitespace, camel_case_types
+
+import 'package:flutter/material.dart';
+import 'package:ig_clone/screens/widgets/stories_bubble.dart';
+import 'package:ig_clone/screens/widgets/user_posts.dart';
+
+class newsFeed extends StatelessWidget {
+  const newsFeed({
+    super.key,
+    required this.people,
+  });
+
+  final List people;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: ListView.builder(
+              scrollDirection: Axis.vertical,
+              itemCount: people.length,
+              itemBuilder: (context, index) {
+                return UserPosts(text: people[index]);
+              }),
+        )
+      ],
+    );
+  }
+}
+
+class storiesBar extends StatelessWidget {
+  const storiesBar({
+    super.key,
+    required this.people,
+  });
+
+  final List people;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 130,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: people.length,
+          itemBuilder: (context, index) {
+            return storiesBubble(text: people[index]);
+          }),
+    );
+  }
+}
